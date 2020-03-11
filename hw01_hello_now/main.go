ea37009584c598bdab2878e0b2dd7f09042b9475
eglebov <evgeniy.glebov@iqoption.com>
2020-03-11 16:24:02 +0300

    implemented hw01 logic

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"time"
+)
+
 func main() {
-	// Place your code here
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatalf("Failed to receive exact time. Reason: %s", err)
+
+		return
+	}
+
+	fmt.Printf("current time: %s\n", time.Now())
+	fmt.Printf("exact time: %s\n", ntpTime)
 }
