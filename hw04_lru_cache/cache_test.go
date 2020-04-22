0c756fbb6b676ceef9116ea2961f6e4493bf3783
eglebov <evgeniy.glebov@iqoption.com>
2020-04-22 16:56:44 +0300

    added test for clear

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -80,6 +80,37 @@ func TestCache(t *testing.T) {
 		require.True(t, ok)
 		require.Equal(t, 555, val)
 	})
+
+	t.Run("clear logic", func(t *testing.T) {
+		c := NewCache(3)
+
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		val, _ := c.Get("aaa")
+		require.Equal(t, 100, val)
+
+		val, _ = c.Get("bbb")
+		require.Equal(t, 200, val)
+
+		val, _ = c.Get("ccc")
+		require.Equal(t, 300, val)
+
+		c.Clear()
+
+		val, ok := c.Get("aaa")
+		require.Nil(t, val)
+		require.False(t, ok)
+
+		val, ok = c.Get("bbb")
+		require.Nil(t, val)
+		require.False(t, ok)
+
+		val, ok = c.Get("ccc")
+		require.Nil(t, val)
+		require.False(t, ok)
+	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
