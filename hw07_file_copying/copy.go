338aaeffb34ebda69ebaf73dc2fbcbeffba07a8c
eglebov <evgeniy.glebov@iqoption.com>
2020-05-28 11:39:42 +0300

    hw07: main logic

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	//"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +13,51 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	src, err := os.Open(fromPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	info, err := os.Stat(fromPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if 0 == info.Size() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dst, err := os.Create(toPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = src.Seek(offset, io.SeekStart)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if limit > 0 {
+		_, err = io.CopyN(dst, src, limit)
+
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+	} else {
+		_, err = io.Copy(dst, src)
+
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+	}
+
 	return nil
 }
