f39742bd5b68fcbc6289dd8e9fbf59823abf09b0
eglebov <evgeniy.glebov@iqoption.com>
2020-06-03 22:47:50 +0300

    hw08 completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,53 @@
 package main
 
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+)
+
+var ErrInvalidDir = errors.New("failed to read directory")
+var ErrEmptyDir = errors.New("empty dir")
+var ErrInvalidFile = errors.New("failed to read file")
+
 type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+
+	res := make(Environment)
+
+	if err != nil {
+		return res, ErrInvalidDir
+	}
+
+	if len(files) < 1 {
+		return res, ErrEmptyDir
+	}
+
+	for _, file := range files {
+		data, err := ioutil.ReadFile(filepath.Join(dir, file.Name()))
+
+		if err != nil {
+			return res, ErrInvalidFile
+		}
+
+		lines := bytes.Split(data, []byte("\n"))
+
+		var value []byte
+
+		if len(lines) > 0 {
+			value = lines[0]
+			value = bytes.Replace(value, []byte("\x00"), []byte("\n"), -1)
+			value = bytes.TrimRight(value, " ")
+			value = bytes.TrimRight(value, "\t")
+		}
+
+		res[file.Name()] = string(value)
+	}
+
+	return res, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+	for k, v := range env {
+		var err error
+
+		if len(v) > 0 {
+			err = os.Setenv(k, v)
+		} else {
+			err = os.Unsetenv(k)
+		}
+
+		if err != nil {
+			log.Fatalf("Failed to set env vars: %s", err)
+
+			return 1
+		}
+	}
+
+	c := exec.Command(cmd[0], cmd[1:]...)
+	c.Env = append(os.Environ())
+	c.Stdout = os.Stdout
+	c.Stdin = os.Stdout
+	c.Stderr = os.Stderr
+
+	if err := c.Run(); err != nil {
+		log.Fatalf("Failed to execute command: %s", err)
+
+		return 1
+	}
+
+	return 0
 }
